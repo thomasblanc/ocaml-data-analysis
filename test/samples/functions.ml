c28badd07d1b9ad561f5d3c15ecc23418edea06e
Thomas Blanc <thomas.blanc@ens-cachan.fr>
2014-03-04 17:40:57 +0100

    Adding function test file (not working yet)

---
+++ test/samples/functions.ml
@@ -0,0 +1,17 @@
+let a = 5+3
+
+let f x = 5+x
+
+let a = f 3
+
+
+
+(* let rec rev acc = function *)
+(*   | h::t -> rev (h::acc) t *)
+(*   | [] -> acc *)
+
+(* let rev l = rev [] l *)
+
+(* let l = [1;2;3;4] *)
+
+(* let l' = rev l *)
