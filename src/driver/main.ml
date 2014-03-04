03d1f5911964930f2a1b16450a3edce84c8fa23c
Pierre Chambart <pierre.chambart@ocamlpro.com>
2014-03-04 19:26:41 +0100

    Print edge attributes in the output dot

--- src/driver/main.ml
+++ src/driver/main.ml
@@ -2,6 +2,45 @@ let ppf = Format.std_formatter
 
 open My_main_args
 
+let print_hedge_attribute ppf hattr =
+  let open Common_types in
+  let aux ppf = function
+    | Prim (prim, _) ->
+      Print_tlambda.primitive ppf prim
+    | App_prep _ ->
+      Format.fprintf ppf "App_prep"
+    | App ->
+      Format.fprintf ppf "App"
+    | Return _ ->
+      Format.fprintf ppf "Return"
+    | Retexn _ ->
+      Format.fprintf ppf "Retexn"
+    | Var _ ->
+      Format.fprintf ppf "Var"
+    | Const _ ->
+      Format.fprintf ppf "Const"
+    | Constraint _ ->
+      Format.fprintf ppf "Constraint"
+    | Lazyforce _ ->
+      Format.fprintf ppf "Lazyforce"
+    | Ccall _ ->
+      Format.fprintf ppf "Ccall"
+    | Send _ ->
+      Format.fprintf ppf "Send"
+  in
+  match hattr with
+  | [] -> ()
+  | [_, hinfo] -> aux ppf hinfo
+  | (_, hinfo) :: t ->
+    aux ppf hinfo;
+    List.iter (fun (_,hinfo) -> Format.fprintf ppf ", %a" aux hinfo)
+      t
+
+let print_attrhedge ppf h attr =
+  Format.fprintf ppf "%a: %a"
+    Tlambda_to_hgraph.T.Hedge.print h
+    print_hedge_attribute attr
+
 let () =
   Arg.parse arg_parser (handle_file ppf)
     "Make sure that all your files are in the right order";
@@ -40,7 +79,7 @@ let () =
           let ppf = Format.formatter_of_out_channel oc in
           Manager.H.print_dot
             ~print_attrvertex:(fun ppf v _ -> Tlambda_to_hgraph.T.Vertex.print ppf v)
-            ~print_attrhedge:(fun ppf h _ -> Tlambda_to_hgraph.T.Hedge.print ppf h)
+            ~print_attrhedge
             ppf result;
           close_out oc
       end;
