6a9fceabfc6f443ba797d5424de122c0af23f90e
Thomas Blanc <thomas.blanc@ens-cachan.fr>
2014-03-06 16:25:05 +0100

    Test that fails oddly, seems that the analysis doesn't go through after a rec definition

--- test/samples/functions.ml
+++ test/samples/functions.ml
@@ -1,17 +1,26 @@
-let a = 5+3
-
 let f x = 5+x
 
 let a = f 3
 
+(* let _ = assert false *)
 
+let rec rev acc = function
+  | h::t -> rev (h::acc) t
+  | [] -> acc
 
-(* let rec rev acc = function *)
-(*   | h::t -> rev (h::acc) t *)
-(*   | [] -> acc *)
+let _ = assert false
 
-(* let rev l = rev [] l *)
+let rev l = rev [] l
 
-(* let l = [1;2;3;4] *)
+let l = [1;2;3;4]
 
-(* let l' = rev l *)
+let l' = rev l
+
+exception E1
+exception E2
+
+let () =
+  match l' with
+  | [] -> raise E1
+  | 5::_ -> raise E2
+  | _ -> assert false
