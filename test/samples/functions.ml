d9d46be53bf283542893374e13c7708c484cdf25
Thomas Blanc <thomas.blanc@ens-cachan.fr>
2014-03-06 17:35:18 +0100

    Adding useless variables

--- test/samples/functions.ml
+++ test/samples/functions.ml
@@ -2,13 +2,17 @@ let f x = 5+x
 
 let a = f 3
 
+(* let fail_detected = 4 *)
 (* let _ = assert false *)
+(* let after_fail = fail_detected *)
 
 let rec rev acc = function
   | h::t -> rev (h::acc) t
   | [] -> acc
 
+let fail_undetected = 5
 let _ = assert false
+let after_fail = fail_undetected
 
 let rev l = rev [] l
 
